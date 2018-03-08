419728e8be2a6af5afa1d58a9e4b7e32b98911a3
Brady Rhora <brady0423@gmail.com>
2018-03-08 05:44:27 -0500

    Slot edit??

--- BradyBot/Slot.cs
+++ BradyBot/Slot.cs
@@ -11,7 +11,6 @@ namespace ASSbot
         double value;
         string symbol;
         string category;
-        string subcategory;
 
         public Slot(string symbol, int value)
         {
@@ -24,7 +23,6 @@ namespace ASSbot
             this.symbol = $":{symbol}:";
             this.value = value;
             this.category = category;
-            subcategory = "none";
         }
 
         public double GetValue() { return value; }
